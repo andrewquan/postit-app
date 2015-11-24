5aef68a9cc183059a449452e55e90379530a10f6
Andrew Quan <andrew.h.quan@gmail.com>
2015-11-23 16:51:51 -0800

    finished lesson 2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_datetime(dt)
+    dt.strftime("%I:%M%p %m/%d/%Y")
+  end
+
+  def fix_url(str)
+    str.starts_with?('http://') ? str : "http://#{str}"
+  end
 end
